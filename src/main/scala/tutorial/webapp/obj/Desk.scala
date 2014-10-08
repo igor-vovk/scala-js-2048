e389df1bda97d174d188a2fda0de2da4b5c8afa7
Igor Vovk <adwolf15@gmail.com>
2014-10-08 11:42:24 +0300

    Introduced custom types; values changed from numbers to Options with numbers to better mark empty values

--- src/main/scala/tutorial/webapp/obj/Desk.scala
+++ src/main/scala/tutorial/webapp/obj/Desk.scala
@@ -2,32 +2,31 @@ package tutorial.webapp.obj
 
 import tutorial.webapp.obj.Movement.Movement
 
-
-class Desk(points: List[List[Int]]) {
+class Desk(points: Field) {
   require(points.forall(_.length == size), "Points are not square size")
 
   def size: Int = points.length
 
-  protected def copy(patch: List[List[Int]] => List[List[Int]]) = new Desk(patch(points))
+  protected def copy(patch: Field => Field) = new Desk(patch(points))
 
-  def apply(x: Int, y: Int): Int = points(x)(y)
+  def apply(x: Int, y: Int): Item = points(x)(y)
 
-  def updated(x: Int, y: Int, value: Int) = copy(p => p.updated(x, p(x).updated(y, value)))
+  def updated(x: Int, y: Int, value: Item) = copy(p => p.updated(x, p(x).updated(y, value)))
 
-  def isEmptyPos(x: Int, y: Int): Boolean = apply(x, y) == Desk.EmptyValue
+  def isEmptyPos(x: Int, y: Int): Boolean = apply(x, y).isEmpty
 
-  def hasEmpty = points.exists(_.contains(Desk.EmptyValue))
+  def hasEmpty = points.exists(_.exists(_.isEmpty))
 
   def horizontalProj: Projection = new Projection {
     override def get(lineNum: Int) = points(lineNum)
 
-    override def updated(lineNum: Int, value: List[Int]) = copy(_.updated(lineNum, value))
+    override def updated(lineNum: Int, row: Row) = copy(_.updated(lineNum, row))
   }
 
   def verticalProj: Projection = new Projection {
     override def get(lineNum: Int) = points.map(row => row(lineNum))
 
-    override def updated(lineNum: Int, value: List[Int]): Desk = copy(_.zipWithIndex.map {
+    override def updated(lineNum: Int, value: Row): Desk = copy(_.zipWithIndex.map {
       case (row, i) => row.updated(lineNum, value(i))
     })
   }
@@ -42,27 +41,19 @@ class Desk(points: List[List[Int]]) {
 }
 
 object Desk {
-  val GameSize = 4
+  def empty(size: Int): Desk = new Desk(List.fill(size, size)(None))
 
-  val EmptyValue: Int = 0
-
-  def empty: Desk = new Desk(List.fill(GameSize, GameSize)(EmptyValue))
-
-  def shift(vector: List[Int]): List[Int] = vector match {
+  def shift(vector: Row): Row = vector match {
     case Nil => Nil
     case n :: Nil => vector
-    case head :: tail =>
-      val thead :: ttail = tail
-
-      if (head == EmptyValue) {
-        tail.find(_ != EmptyValue).fold(vector) { case nonEmptyEl =>
-          nonEmptyEl :: shift(tail.updated(tail.indexOf(nonEmptyEl), EmptyValue))
-        }
-      } else if (head == thead) {
-        (head * thead) :: shift(ttail) ++ (EmptyValue :: Nil)
-      } else {
-        head :: shift(tail)
+    case None :: tail =>
+      tail.find(_.nonEmpty).fold(vector) { case nonEmptyEl =>
+        nonEmptyEl :: shift(tail.updated(tail.indexOf(nonEmptyEl), None))
       }
+    case Some(a) :: Some(b) :: tail if a == b =>
+      Some(a * b) :: shift(tail) ++ (None :: Nil)
+    case head :: tail =>
+      head :: shift(tail)
   }
 
   def mkMove(initial: Desk, move: Movement): Desk = {
@@ -81,17 +72,17 @@ object Desk {
 }
 
 trait Projection {
-  def get(lineNum: Int): List[Int]
+  def get(lineNum: Int): Row
 
   def apply(lineNum: Int) = get(lineNum)
 
-  def updated(lineNum: Int, value: List[Int]): Desk
+  def updated(lineNum: Int, value: Row): Desk
 }
 
 object Generators {
 
   def startNumber = Iterator.continually {
-    if (math.random < 0.9) 2 else 4
+    Some(if (math.random < 0.9) 2 else 4)
   }
 
   def position(size: Int) = {
@@ -100,7 +91,7 @@ object Generators {
     Iterator.continually((rand, rand))
   }
 
-  def defaultValues(initial: Desk = Desk.empty) =
+  def defaultValues(initial: Desk) =
     Iterator.iterate(initial) { case desk =>
       if (desk.hasEmpty) {
         val (x, y) = position(desk.size).filter((desk.isEmptyPos _).tupled).next()
---
+++ src/main/scala/tutorial/webapp/obj/package.scala
@@ -0,0 +1,11 @@
+package tutorial.webapp
+
+package object obj {
+
+  type Item = Option[Int]
+
+  type Row = List[Item]
+
+  type Field = List[Row]
+
+}
